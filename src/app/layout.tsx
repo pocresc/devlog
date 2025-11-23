f923690e5ac23b26dffd9dac714e41f5368da5a6
gardenny <mompapa7@gmail.com>
2025-11-23 21:18:52 +0900

    프로젝트 초기화

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Chresendo | 최정원 블로그',
+  description: '개발 중에 겪은 이슈와 기억하고 싶은 내용을 기록하고 정리하는 공간',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
